35165182f0635e68da85d6ecaeb7faad9ebcc4e5
Jacek <Jacek@BERLIN>
2020-02-23 08:13:08 +0100

    vv

--- solution-code-spring-mvc-reading-html-form-data-Udemy12-111/src/pl/jaceksysiak/springdemo/mvc/HelloWorldController.java
+++ solution-code-spring-mvc-reading-html-form-data-Udemy12-111/src/pl/jaceksysiak/springdemo/mvc/HelloWorldController.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
-	
 	@RequestMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
 		
 	// need a controller method to process the HTML form
-	
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
